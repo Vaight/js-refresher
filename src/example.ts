94be855683a23a51e6394609f689b88e6d2be4ba
Vaight <thatvaight@gmail.com>
2025-01-15 10:43:01 -0700

    pp

--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,31 @@
 // npx ts-node src/example.ts
 let penis = "pp";
 console.log(penis);
+
+type Person = { // typescript specific
+    firstName: string; // required
+    lastName?: string; // optional (?)
+    age?: number       // optional (?)
+}
+
+const smallObj: Person = { // extends Person
+    firstName: "John",
+    lastName: "Penis" // removing this doesn't matter 
+}
+
+// code like its 1995
+function addNumbers(numA, numB) {
+    return numA + numB;
+}
+
+// arrow function
+const addThings = (numA: number, numB: number): number => {
+    return numA + numB;
+};
+
+// simple functions like this are faster
+const addStuff = (numA, numB) => numA + numB
+
+console.log(addNumbers(1,2));
+console.log(addThings(1,2));
+console.log(addStuff(1,2));
